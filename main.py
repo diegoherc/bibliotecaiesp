3ab235c6e2c04e79b85f2db0e7c19f1a53084654
Diego Herculano <diegoherculanojp@gmail.com>
2020-10-18 22:27:37 -0300

    Começo

---
+++ main.py
@@ -0,0 +1,27 @@
+livros = []
+
+def cadastrarNovoLivro(livro):
+    livros.append(livro)
+
+def cadastrarCategoriaTematica():
+
+def addTitulo():
+
+def attQntTitulo():
+
+def removerTitulo():
+
+def buscarExemplar():
+
+def importarDados():
+
+def obterStatus(livro):
+
+def gerarRelatorio():
+
+def gerarRelatorioCategoria():
+
+def gerarRelatorioTematica():
+
+if __name__ == '__main__':
+
