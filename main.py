3b0762fd178b76e9cb07804d5951d47f18fad07e
Diego Herculano <diegoherculanojp@gmail.com>
2020-10-18 22:43:02 -0300

    Começo

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 livros = []
 
-def cadastrarNovoLivro(livro):
-    livros.append(livro)
 
-def cadastrarCategoriaTematica():
+def cadastrarNovoLivro(livro, categoria, ano):
+    global livros
+    livros.append( {
+        "nome": livro,
+        "categoria": categoria,
+        "ano": ano
+    } )
 
-def addTitulo():
-
-def attQntTitulo():
-
-def removerTitulo():
-
-def buscarExemplar():
-
-def importarDados():
-
-def obterStatus(livro):
-
-def gerarRelatorio():
-
-def gerarRelatorioCategoria():
-
-def gerarRelatorioTematica():
+#
+# def cadastrarCategoriaTematica():
+#
+# def addTitulo():
+#
+# def attQntTitulo():
+#
+# def removerTitulo():
+#
+# def buscarExemplar():
+#
+# def importarDados():
+#
+# def obterStatus(livro):
+#
+# def gerarRelatorio():
+#
+# def gerarRelatorioCategoria():
+#
+# def gerarRelatorioTematica():
+#
 
 if __name__ == '__main__':
-
+    cadastrarNovoLivro('Harry Potter', 'Viadagem', '1995')
+    cadastrarNovoLivro('Crepusculo', 'Viadagem', '1924')
+    cadastrarNovoLivro('A culpa é das estrelas', 'Viadagem', '9999')
